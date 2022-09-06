1591af60e296f92855d66af729f34ab98878a242
Delmin <romanovych123@gmail.com>
2022-09-06 21:44:20 +0300

    Parametrized files using argparse

--- predict.py
+++ predict.py
@@ -1,34 +1,35 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from train import IMAGES_LIST
-trained_model = tf.keras.models.load_model("trained_model10000x9", compile=False)
+import argparse
+
+trained_model = tf.keras.models.load_model("trained_model", compile=False)
 
 
-def predict_from_image(img_path, visualize=True):
+def predict_from_image(img_path, compare):
     """
     Using trained model to predict ship locations
     Args:
         img_path: string path to an image
-        visualize: True if you want to save plot with image and prediction to a predictions folder
+        compare: True if you want to save plot image and prediction and False if you want to save just mask
 
     Returns:
-        prediction: NumPy (256x256) array representation of predicted image
+        prediction: NumPy (96x96) array representation of predicted image
     """
-    filename=img_path.split(sep='/')[-1]
+    filename = img_path.split(sep='/')[-1]
     # Process image
     pred_img = tf.io.read_file(img_path)
     pred_img = tf.image.decode_jpeg(pred_img, channels=3)
-    pred_img = tf.image.resize(pred_img, (256, 256))
+    pred_img = tf.image.resize(pred_img, (96, 96))
     pred_img = tf.cast(pred_img, tf.float32) / 255.
-    pred_img = tf.reshape(pred_img, (1, 256, 256, 3))
+    pred_img = tf.reshape(pred_img, (1, 96, 96, 3))
     # Make a prediction
     prediction = trained_model.predict(pred_img)
-    prediction = prediction.reshape(256, 256)
+    prediction = prediction.reshape(96, 96)
     # Create plot with image and prediction
-    if visualize:
+    if compare:
         fig = plt.figure()
         fig.add_subplot(1, 2, 1)
-        plt.imshow(pred_img.numpy().reshape(256, 256, 3))
+        plt.imshow(pred_img.numpy().reshape(96, 96, 3))
         plt.axis('off')
         plt.title(f"Image")
         fig.add_subplot(1, 2, 2)
@@ -37,9 +38,17 @@ def predict_from_image(img_path, visualize=True):
         plt.title(f"Prediction")
         # Save figure
         plt.savefig("predictions/predicted_" + filename, bbox_inches='tight')
+    else:
+        plt.imsave("predictions/predicted_mask_" + filename, prediction)
     return prediction
 
 
-# Predictions for last 100 images
-for image_path in IMAGES_LIST[-100:]:
-    predict_from_image(image_path, visualize=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('img', type=str, help='image path')
+parser.add_argument('-compare_flag', type=str, default='True')
+args = parser.parse_args()
+if args.compare_flag.lower() in ['f', 'false', 'n', 'no']:
+    predict_from_image(args.img, compare=False)
+else:
+    predict_from_image(args.img, compare=args.compare_flag)
+
--- train.py
+++ train.py
@@ -5,13 +5,7 @@ import matplotlib.pyplot as plt
 import os
 from model import unet_model
 from model import DiceLoss
-
-# CONSTANTS
-IMAGE_SHAPE = (768, 768)
-path = ''
-IMAGE_PATH = os.path.join(path, 'data/Images/')
-IMAGES_LIST = os.listdir(IMAGE_PATH)
-IMAGES_LIST = sorted([IMAGE_PATH + i for i in IMAGES_LIST])
+import argparse
 
 
 def rle_to_mask(rle):
@@ -50,38 +44,79 @@ def image_mask_gen():
     for i in images_range:
         img = tf.io.read_file(IMAGES_LIST[i])
         img = tf.image.decode_jpeg(img, channels=3)
-        img = tf.image.resize(img, (256, 256))
-        img = tf.cast(img, tf.float32) / 255.   # Normalizing input
+        img = tf.image.resize(img, (96, 96))
+        img = tf.cast(img, tf.float32) / 255.  # Normalizing input
         mask = rle_to_mask(df['EncodedPixels'].iloc[i])
-        mask = tf.image.resize(mask, (256, 256))
+        mask = tf.image.resize(mask, (96, 96))
         yield (img, mask)
 
 
 if __name__ == "__main__":
-    df = pd.read_csv('data/test_ship_segmentations_v3.csv')
+    # Inout image shape constant
+    IMAGE_SHAPE = (768, 768)
+
+    # Get paths for images folder and masks csv file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('img', type=str, help='train images folder')
+    parser.add_argument('csv', type=str, help='csv file path')
+    args = parser.parse_args()
+
+    # Create and preprocess dataframe from csv file
+    df = pd.read_csv(args.csv)
     df['EncodedPixels'] += ' '
     df = df.groupby(['ImageId']).sum()
     df['EncodedPixels'] = df['EncodedPixels'].replace(0, '')
+    print(len(df))
+
+    # Create images list from an img folder
+    path = ''
+    IMAGE_PATH = os.path.join(path, args.img)
+    IMAGES_LIST = os.listdir(IMAGE_PATH)
+    IMAGES_LIST = sorted([IMAGE_PATH + i for i in IMAGES_LIST])
 
     # DOWNSAMPLE IMAGES WITH NO SHIPS
     images_range = [index for index in range(len(df)) if not (
-                index % 3 and df['EncodedPixels'].iloc[index] == '')]  # Leaving approx 33% of images without ship
+            index % 3 and df['EncodedPixels'].iloc[index] == '')]  # Leaving approx 33% of images without ship
 
-    training = tf.data.Dataset.from_generator(image_mask_gen,
-                                              output_types=(tf.float32, tf.float32),
-                                              output_shapes=((256, 256, 3), (256, 256, 1)))
+    ds = tf.data.Dataset.from_generator(image_mask_gen,
+                                        output_types=(tf.float32, tf.float32),
+                                        output_shapes=((96, 96, 3), (96, 96, 1)))
+
+    # Splitting data 70%/15%/15% training/validation/test sets
+    train_size = int(len(images_range) * 0.7)
+    val_size = int(len(images_range) * 0.15)
+    train_ds = ds.take(train_size)
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
 
     unet = unet_model()
     print(unet.summary())
-    unet.compile(optimizer='adam',
+    optimizer = tf.keras.optimizers.Adam(0.0007)
+    unet.compile(optimizer=optimizer,
                  loss=DiceLoss(),
-                 metrics=tf.keras.metrics.BinaryIoU())
+                 metrics=[tf.keras.metrics.BinaryIoU()])
 
-    EPOCHS = 9
-    BATCH_SIZE = 16
-    train_dataset = training.batch(BATCH_SIZE)
-    print(train_dataset.element_spec)
-    model_history = unet.fit(train_dataset, epochs=4)
-    plt.plot(model_history.history['loss'])
-    plt.savefig("logs/model_loss15000x4.png")
-    unet.save("trained_model15000x4")
+    EPOCHS = 24
+    BATCH_SIZE = 32
+
+    train_dataset = train_ds.batch(BATCH_SIZE)
+    val_dataset = val_ds.batch(BATCH_SIZE)
+    test_dataset = test_ds.batch(BATCH_SIZE)
+
+    print(train_ds.element_spec)
+    model_history = unet.fit(train_dataset, epochs=EPOCHS, validation_data=val_dataset)
+    test = unet.evaluate(test_dataset)
+    print(test)
+
+    plt.plot(model_history.history['loss'], label='loss')
+    plt.plot(model_history.history['val_loss'], label='val_loss')
+    plt.legend()
+    plt.savefig("logs/model_loss.png")
+    plt.close()
+    plt.plot(model_history.history['binary_io_u'], label='iou')
+    plt.plot(model_history.history['val_binary_io_u'], label='val_iou')
+    plt.legend()
+    plt.savefig("logs/model_iou.png")
+
+    unet.save("trained_model")
+    unet.save_weights('trained_model_weights.h5')
